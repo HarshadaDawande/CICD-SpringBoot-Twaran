6afaf76aa3a1a292078c102038429decccc3b154
harshada-dawande <harshada.dawande@thoughtworks.com>
2022-03-02 08:39:15 +0530

    Add hello message for twaran

--- src/main/java/com/tw/nodocker/demo/HelloController.java
+++ src/main/java/com/tw/nodocker/demo/HelloController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/")
 public class HelloController {
+    @GetMapping("/hello-twaran/")
+    public String sayHelloTwaran() {
+        return "Hello Twaran 2, welcome to no docker demo";
+    }
     @GetMapping("/hello/")
     public String sayHello() {
         return "Hello world, welcome to no docker demo";
