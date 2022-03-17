a2ade655393c288b597c67aaa14a7d7bca7bac09
harshada-dawande <harshada.dawande@thoughtworks.com>
2022-03-17 20:56:29 +0530

    Refactor message 2

--- src/main/java/com/tw/nodocker/demo/HelloController.java
+++ src/main/java/com/tw/nodocker/demo/HelloController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.*;
 public class HelloController {
     @GetMapping("/hello/")
     public String sayHelloTwaran() {
+
+
         return "Hello Twaran, welcome to CICD Spring Boot demo";
     }
     @GetMapping("/")
     public String sayHello() {
+
+
         return "Hello Twaran, welcome to demo";
     }
 }
